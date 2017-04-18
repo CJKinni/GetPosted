5c6667f441724b8f77c0926fd74e6d157b2ddf84
Christopher J Kinniburgh <s.github@cjkinni.com>
2017-04-17 21:44:26 -0500

    Update home.rb

--- home.rb
+++ home.rb
@@ -15,15 +15,15 @@ get '/' do
   <br>
   This is an easy way to automatically fill out a form that only accepts POST requests.<br>
   If you wanted to email someone a link to sign into a website with a guest account,<br>
-  you couldn't embed a completed form in an email (Almost no email client supports forms.)<br>
-  Instead, you could include an email to a GETPOSTed url that redirects to submit a<br>
-  completed form.<br>
+  you couldn't embed a completed form in an email (no modern e-mail client supports forms.)<br>
+  Instead, you can include an email to a GETPOSTed url that will redirect the recipient's<br>
+  browser to submit a completed form via POST.<br>
   <br>
   This service works by providing a hidden form that is built on page-load and uses javascript<br>
   to 'click' the form submission.  This means cookies will work correctly for the site you've<br> 
   submitted to, but you need to have a javascript enabled browser to use the service.<br>
   <br>
-  Note:  Make sure not to use this service for sensitive information.<br>
+  <b>Note</b>:  Make sure not to use this service for sensitive information.<br>
   If you're not comfortable with anyone seeing what you're sending, don't send it<br>
   via a website you've never heard of.<br>
   <br>
