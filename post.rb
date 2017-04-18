c146fa9b2f24a70a42dd29de9dc3df05e78bc65e
Christopher J Kinniburgh <s.github@cjkinni.com>
2017-04-17 21:54:21 -0500

    Update post.rb

--- post.rb
+++ post.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 get '/post' do
 
   form = "<form action=\"#{params[:action].to_s}\" method=\"post\">"
-  params = params.to_h.max_by(100, &:last).to_h
+  #params = params.to_h.max_by(100, &:last).to_h
   params.each do |k,v|
     form += "<input type=\"hidden\" name=\"#{k}\" value=\"#{v}\">"
   end
