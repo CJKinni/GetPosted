55040be3746629f365ce994afaa7c71519e28b62
Christopher J Kinniburgh <s.github@cjkinni.com>
2017-04-21 11:44:23 -0500

    Link to GitHub

--- home.rb
+++ home.rb
@@ -45,7 +45,8 @@ http://getposted.io/post?action=http://httpbin.org/post&key=value&key2=value2</a
 <br>
 A project by <a href=\"http://twitter.com/cjkinni\">@CJKinni</a><br>
 <br>
-Support via Bitcoin: 13PkMB9vGgtGabmxPrLpL5itPyJzuFL8tR
+Support via Bitcoin: 13PkMB9vGgtGabmxPrLpL5itPyJzuFL8tR<br>
+<a href="https://github.com/CJKinni/GetPosted">View on GitHub</a>
 </body>
 </html>"
 end
