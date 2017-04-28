f319fb4ca1a50da025d6f3c74a39c9905461fafc
Christopher J Kinniburgh <s.github@cjkinni.com>
2017-04-28 09:11:03 -0500

    Sanitize params

--- post.rb
+++ post.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
+require 'sanitize'
 
 get '/post' do
 
-  form = "<form action=\"#{params[:action].to_s}\" method=\"post\">"
+  form = "<form action=\"#{Sanitize.fragment(params[:action].to_s)}\" method=\"post\">"
   params.each do |k,v|
-    form += "<input type=\"hidden\" name=\"#{k}\" value=\"#{v}\">"
+    form += "<input type=\"hidden\" name=\"#{Sanitize.fragment(k)}\" value=\"#{Sanitize.fragment(v)}\">"
   end
   form += "<button style=\"display: none;\" id=\"f\" type=\"submit\"> </button>"
   form += "</form>"
